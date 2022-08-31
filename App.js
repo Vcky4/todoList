d401278ff24a3536527f441da3c88d6f6adde3c5
Vcky4 <okon.victor.u@gmail.com>
2022-08-31 15:40:50 +0100

    complete list and add text input

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [task, setTask] = useState();
+
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask('');
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
       {/* Today's Task */}
@@ -11,10 +27,29 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go! */}
-          <Task text = {'task 1'} />
-          <Task text = {'task 2'} />
+          {
+            taskItems.map((item, index) => {
+              return <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            })
+          }
+
         </View>
       </View>
+
+      {/* write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -34,5 +69,34 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  addText: {
+    fontSize: 30,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 30,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
 });
